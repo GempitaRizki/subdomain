092d15ba96645cebdffa6df5382b5930bcab13c6
duplay-informatika <grrstudiocreative@gmail.com>
2025-12-05 22:56:16 +0700

    [welcome]

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Dashboard\IndexController;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+
+Route::get('/dashboard-menu', [IndexController::class, 'index'])->name('dashboard.menu');
